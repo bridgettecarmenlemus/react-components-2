3bb7ffd8e5e973f954bdfa5226ffd474ee75e0ba
Bridgette Lemus <bridgette.lemus.se@gmail.com>
2022-08-09 11:07:40 -0400

    created a beer list

---
+++ react-lifecycle/src/components/Beers/index.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+export default function Beers () {
+    const [beerList, setBeerList] = useState();
+    const [beerType, setBeerType] = useState('ale');
+    useEffect(() => {
+        fetch(`https://api.sampleapis.com/beers/${beerType}`) // ale, stouts
+        .then(response => response.json())
+        .then(beers => setBeerList(beers))
+        .catch(alert);
+    }, [beerType])
+    if (!beerList) {
+        return <h2>Loading . . .</h2>
+    }
+    return (
+        <>
+            <button onClick={() => setBeerType('ale')}>Ale</button>        
+            <button onClick={() => setBeerType('stouts')}>Stouts</button>              
+        <ul>
+            {beerList.map(beer => (
+                <li key ={beer.id}>{beer.name}</li>
+            ))}
+        </ul>
+        </>
+    )
+}
+
