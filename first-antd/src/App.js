9378033234bce865056ee561c38aa9a4160f0c25
Bridgette Lemus <bridgette.lemus.se@gmail.com>
2022-08-15 10:20:38 -0400

    created a small login and password form using an Ant design library

---
+++ first-antd/src/App.js
@@ -0,0 +1,14 @@
+import "antd/dist/antd.css";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <>
+      <h1>My First Ant Design</h1>
+      <Login />
+  
+    </>
+  );
+}
+
+export default App;
